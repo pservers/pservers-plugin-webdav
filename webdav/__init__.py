fde4272e3f9db2a46e0836cadd90174b20f084b9
fpemud <fpemud@sina.com>
2021-03-09 16:59:07 +0800

    fix

--- webdav/__init__.py
+++ webdav/__init__.py
@@ -2,25 +2,24 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
 import os
+import pathlib
 import hashlib
 
 
 """
 Access:
-  URL                 USER        EFFECT
-  http://...                      filemanager-ui,read-only
-  http://...          ro          filemanager-ui,read-only
-  http://...          rw          filemanager-ui,read-write
-  http://.../pub                  httpdir
-  http://.../pub      ro          httpdir
-  http://.../pub      rw          httpdir
-  http://.../dav                  webdav,read-only
-  http://.../dav      ro          webdav,read-only
-  http://.../dav      rw          webdav,read-write
+  PROTOCOL      URL                 USER        EFFECT
+  http          http://...                      filemanager-ui,read-only
+  http          http://...          ro          filemanager-ui,read-only
+  http          http://...          rw          filemanager-ui,read-write
+  http          http://.../pub                  httpdir
+  webdav        http://.../dav                  webdav,read-only
+  webdav        http://.../dav      ro          webdav,read-only
+  webdav        http://.../dav      rw          webdav,read-write
 
 Notes:
   1. We don't support ftp-protocol since it does not support one-server-multiple-domain.
-  2. We have to use "pub" and "dav" subdirectory. Static files for filemanager-ui would conflict with the files being served if not doing so.
+  2. We have to use "pub" and "dav" subdirectory. Static files of filemanager-ui would conflict with the files being served if not doing so.
 """
 
 
@@ -42,18 +41,8 @@ def start(params):
     # wsgi script
     wsgiFn = os.path.join(tmpDir, "wsgi-%s.py" % (serverId))
     with open(wsgiFn, "w") as f:
-        buf = ''
-        buf += '#!/usr/bin/python3\n'
-        buf += '# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-\n'
-        buf += '\n'
-        buf += 'from wsgidav.fs_dav_provider import FilesystemProvider\n'
-        buf += 'from wsgidav.wsgidav_app import WsgiDAVApp\n'
-        buf += '\n'
-        buf += 'config = {\n'
-        buf += '    "provider_mapping": {"/": FilesystemProvider(%s)}\n' % (webdavDir),
-        buf += '    "verbose": 1,\n'
-        buf += '    }\n'
-        buf += 'application = WsgiDAVApp(config)\n'
+        buf = pathlib.Path(os.path.join(selfDir, "entry.py.in")).read_text()
+        buf = buf.replace("%%WEBDAV_DIR%%", webdavDir)
         f.write(buf)
 
     # generate apache config segment
@@ -81,11 +70,3 @@ def start(params):
 
 def stop(private_data):
     pass
-
-
-class _Util:
-
-    @staticmethod
-    def ensureDir(dirname):
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
